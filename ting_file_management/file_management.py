8ef67a9d275ce45211f1a2615c5c479fce824462
Bruno Trindade <bruno@DESKTOP-I1C99NP.localdomain>
2022-07-31 11:06:37 -0300

    feat: req. 2 - file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+import os
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if(path_file[-3:] != "txt"):
+        print("Formato inválido", file=sys.stderr)
+    else:
+        if not os.path.exists(path_file):
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        else:
+            with open(path_file, 'r', encoding="utf-8") as file:
+                lines = file.readlines()
+                return [line.replace("\n", "") for line in lines]
